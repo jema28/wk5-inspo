9946a326cfda58a6121de594ee5cfa46383d6774
Matt Lubel <matt_lubel@hotmail.com>
2017-03-22 16:49:21 +0200

    complete getQuestions function

--- public/index.js
+++ public/index.js
@@ -1,13 +1,16 @@
 var currentTag = 'all';
 
 // array of buttons
-var optionButtons;
+var optionButtons = document.getElementsByClassName('tag-button');
 
 optionButtons.forEach(function(button) {
   button.addEventListener('click', function(e) {
     // find out which button was clicked
     // update the currentTag variable
     // after you chose the right opition we show the right data(run getQuestions function)
+    getQuestions(e.target.tag, function(err, response) {
+      console.log(response);
+    });
   });
 });
 
@@ -19,4 +22,17 @@ function renderToList(error, items) {
 function getQuestions(tag, callback) {
   // tag is e.g. 'all' or 'js'
   // callback fires after response is received
+  var url = '/questions?tag=' + tag;
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      callback(null, JSON.parse(xhr.responseText));
+    }
+    else if (xhr.status === 500) {
+      var errorMessage = xhr.responseText;
+      callback(errorMessage, null);
+    }
+  }
+  xhr.open('GET', url);
+  xhr.send();
 }
