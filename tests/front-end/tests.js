5464bd8e8931f71492d8f6dd4f1f0a2bcc54d93e
mohamed omarii <mohamed.44301@hotmail.com>
2017-03-22 17:48:23 +0200

    test the render function

--- tests/front-end/tests.js
+++ tests/front-end/tests.js
@@ -1,3 +1,12 @@
 QUnit.test("test QUnit is working", function(assert) {
   assert.ok(true, "test should pass");
 });
+
+QUnit.test("test render function", function(assert){
+  var section = document.getElementById('posts-container');
+  assert.equal(section.innerHTML, '', 'posts-container is empty');
+  renderToList(null, mockRenderData);
+  assert.equal(section.childNodes.length,1,'render function add ul');
+  var ulElement = section.childNodes[0];
+  assert.equal(ulElement.childNodes.length, 10, 'should add 10 elements to ul');
+});
