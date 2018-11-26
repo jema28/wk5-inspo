1c5338c289e9c1fca9e514805d5ea16e304b384c
Jemila <jemila.abulhawa@gmail.com>
2018-11-26 10:24:40 +0200

    add eslint and fix issues

--- src/handlers.js
+++ src/handlers.js
@@ -1,91 +1,109 @@
-var fs = require('fs');
-var url = require('url');
+var fs = require('fs')
+var url = require('url')
+var path = require('path')
 
-var requestModule = require('request');
+var requestModule = require('request')
 
-var NUM_RESULTS = 10;
+var NUM_RESULTS = 10
 
 var headers = {
-  'html': {'content-type' : 'text/html'},
-  'css': {'content-type' : 'text/css'},
-  'js': {'content-type' : 'application/js'},
-  'json': {'content-type' : 'application/json'},
-  'plain': {'content-type' : 'text/plain'}
-};
-
-var handlers = {};
-
-handlers.home = function(request, response) {
-  fs.readFile(__dirname + '/../public/index.html', function(error,file) {
-    if (error) {
-      response.writeHead(500, headers.plain);
-      response.end('something went wrong!');
-      return;
-    }
-    response.writeHead(200, headers.html);
-    response.end(file);
- });
+  'html': {
+    'content-type': 'text/html'
+  },
+  'css': {
+    'content-type': 'text/css'
+  },
+  'js': {
+    'content-type': 'application/js'
+  },
+  'json': {
+    'content-type': 'application/json'
+  },
+  'plain': {
+    'content-type': 'text/plain'
+  }
 }
 
-handlers.style = function(request, response) {
-  fs.readFile(__dirname + '/../public/style.css', function(error,file) {
-    if (error) {
-      response.writeHead(500, headers.plain);
-      response.end('something went wrong!');
-      return;
-    }
-    response.writeHead(200, headers.css);
-    response.end(file);
- });
+var handlers = {}
+
+handlers.home = function (request, response) {
+  fs
+    .readFile(path.join(__dirname, '..', 'public', 'index.html'), function (error, file) {
+      if (error) {
+        response.writeHead(500, headers.plain)
+        response.end('something went wrong!')
+        return
+      }
+      response.writeHead(200, headers.html)
+      response.end(file)
+    })
 }
 
-handlers.index = function(request, response) {
-  fs.readFile(__dirname + '/../public/index.js', function(error,file) {
-    if (error) {
-      response.writeHead(500, headers.plain);
-      response.end('something went wrong!');
-      return;
-    }
-    response.writeHead(200, headers.js);
-    response.end(file);
- });
+handlers.style = function (request, response) {
+  fs
+    .readFile(path.join(__dirname, '..', 'public', 'style.css'), function (error, file) {
+      if (error) {
+        response.writeHead(500, headers.plain)
+        response.end('something went wrong!')
+        return
+      }
+      response.writeHead(200, headers.css)
+      response.end(file)
+    })
 }
 
-handlers.questions = function(request, response) {
+handlers.index = function (request, response) {
+  fs
+    .readFile(path.join(__dirname, '..', 'public', 'index.js'), function (error, file) {
+      if (error) {
+        response.writeHead(500, headers.plain)
+        response.end('something went wrong!')
+        return
+      }
+      response.writeHead(200, headers.js)
+      response.end(file)
+    })
+}
+
+handlers.questions = function (request, response) {
   // get tag
-  var parsedURL = url.parse(request.url, true);
-  var tag = parsedURL.query.tag;
+  var parsedURL = url.parse(request.url, true) // eslint-disable-line node/no-deprecated-api
+  var tag = parsedURL.query.tag
 
   var tags = {
     'all': '',
     'js': '&tagged=js',
     'node': '&tagged=nodejs'
-  };
+  }
 
   // make request
-  var stackURL = 'https://api.stackexchange.com/2.2/questions?order=desc&sort=creation&site=stackoverflow' + tags[tag];
+  var stackURL = 'https://api.stackexchange.com/2.2/questions?order=desc&sort=creation&site=stacko' +
+      'verflow' + tags[tag]
 
   var reqOptions = {
     uri: stackURL,
-    gzip: true /* EFFING HELL */
+    gzip: true/* EFFING HELL */
   }
 
-  requestModule(reqOptions, function(error, res, body) {
+  requestModule(reqOptions, function (error, res, body) {
     if (error) {
-      response.writehead(500, headers.plain);
-      response.end('Server error! Holy moly!');
+      response.writehead(500, headers.plain)
+      response.end('Server error! Holy moly!')
     }
 
     // format and send response
-    var data = JSON.parse(body).items.slice(0, NUM_RESULTS);
-    response.writeHead(200, headers.json);
-    response.end(JSON.stringify(data));
-  });
+    var data = JSON
+      .parse(body)
+      .items
+      .slice(0, NUM_RESULTS)
+    response.writeHead(200, headers.json)
+    response.end(JSON.stringify(data))
+  })
 }
 
-handlers.notFound = function(request, response) {
-  response.writeHead(404, headers.plain);
-  response.end('Resource not found');
+handlers.notFound = function (request, response) {
+  response.writeHead(404, headers.plain)
+  response.end('Resource not found')
 }
 
-module.exports = handlers;
+module.exports = handlers
--- tests/back-end/tests.js
+++ tests/back-end/tests.js
@@ -1,72 +1,70 @@
-const test = require('tape');
-const shot = require('shot');
-const router = require('../../src/router.js');
-const fs = require('fs');
-const path = require('path');
+const test = require('tape')
+const shot = require('shot')
+const router = require('../../src/router.js')
+const fs = require('fs')
+const path = require('path')
 
 test('Initialise', (t) => {
-  var num = 2;
-  t.equal(num, 2, 'Should return 2');
-  t.end();
-});
+  var num = 2
+  t.equal(num, 2, 'Should return 2')
+  t.end()
+})
 
-test('Home route', function(t) {
-
+test('Home route', function (t) {
   var filePath = path.join(__dirname, '../..', 'public/index.html')
-  var indexFile = fs.readFileSync(filePath).toString();
+  var indexFile = fs.readFileSync(filePath).toString()
   const reqOptions = {
     method: 'get',
     url: '/'
   }
 
-  shot.inject(router, reqOptions, function(res) {
-    t.equal(res.statusCode, 200, 'should respond with status code of 200');
-    t.equal(res.headers['content-type'], 'text/html', 'content-type is html text');
+  shot.inject(router, reqOptions, function (res) {
+    t.equal(res.statusCode, 200, 'should respond with status code of 200')
+    t.equal(res.headers['content-type'], 'text/html', 'content-type is html text')
     t.equal(res.payload, indexFile, 'index.html HERE')
-    t.end();
-  });
-});
+    t.end()
+  })
+})
 
-test('Unknown route test', function(t) {
-  const reqOptions={
+test('Unknown route test', function (t) {
+  const reqOptions = {
     method: 'get',
     url: '/hiba'
   }
 
-  shot.inject(router,reqOptions,function(res) {
-    t.equal(res.statusCode,404,'should respond with status code of 404');
-    t.equal(res.headers['content-type'],'text/plain','content-type is plain text');
-    t.end();
-  });
-});
+  shot.inject(router, reqOptions, function (res) {
+    t.equal(res.statusCode, 404, 'should respond with status code of 404')
+    t.equal(res.headers['content-type'], 'text/plain', 'content-type is plain text')
+    t.end()
+  })
+})
 
-test('css style route test', function(t) {
-  const reqOptions={
+test('css style route test', function (t) {
+  const reqOptions = {
     method: 'get',
     url: '/style.css'
   }
 
-  shot.inject(router,reqOptions,function(res){
-    t.equal(res.statusCode,200,'should respond with status code of 200');
-    t.equal(res.headers['content-type'],'text/css');
-    t.end();
-  });
-});
+  shot.inject(router, reqOptions, function (res) {
+    t.equal(res.statusCode, 200, 'should respond with status code of 200')
+    t.equal(res.headers['content-type'], 'text/css')
+    t.end()
+  })
+})
 
-test('index js route test', function(t) {
-
+test('index js route test', function (t) {
   var filePath = path.join(__dirname, '../..', 'public/index.js')
-  var indexJsFile = fs.readFileSync(filePath).toString();
+  var indexJsFile = fs.readFileSync(filePath).toString()
 
-  const reqOptions={
+  const reqOptions = {
     method: 'get',
     url: '/index.js'
   }
 
-  shot.inject(router,reqOptions,function(res){
-    t.equal(res.statusCode,200,'should respond with status code of 200');
-    t.equal(res.headers['content-type'],'application/js');
+  shot.inject(router, reqOptions, function (res) {
+    t.equal(res.statusCode, 200, 'should respond with status code of 200')
+    t.equal(res.headers['content-type'], 'application/js')
     t.equal(res.payload, indexJsFile, 'index.js HERE ')
-    t.end();
-  });
-});
+    t.end()
+  })
+})
